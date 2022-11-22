96316b0c64a14999f043ece3e02cb905a20e4e2e
paulohmoreira <phmoreira37@gmail.com>
2022-11-22 13:33:00 -0300

    resolução desafio parte-1 1.3

---
+++ parte-1/desafio1.3.js
@@ -0,0 +1,30 @@
+// Primeiro passo: Desativar o modal e dropdown
+
+// Seleciona a div dropdown com os números
+const divItensWpp = document.getElementById("header-card-whatsapp");
+// Seleciona o modal "NEGOCIE PELO WHATSAPP"
+const modalWpp = document.getElementById("new-vehicles-showcase_modal-whatsapp-box");
+
+// Remove classe micromodal-slide para o modal não abrir mais
+modalWpp.classList.remove("micromodal-slide");
+
+// Remove a div do menu dropdown
+divItensWpp.remove();
+
+
+// Segundo passo: Setar evento click para abrir o Whatsapp em outra página
+
+// Selecionando os dois botões que vão receber o evento
+const btn1 = document.querySelector(".btn.button.button--whatsapp.header__phones-button-toggler")
+const btn2 = document.getElementById("new-vehicles-showcase_cta-box-button")
+
+// Função que cria o evento (Criei uma função para reutilizar o código)
+function setNewEvent(btn){
+  btn.setAttribute(
+    "onclick",
+    `window.open("https://api.whatsapp.com/send?phone=5511999999999","_blank")`
+  );
+}
+
+setNewEvent(btn1);
+setNewEvent(btn2);
